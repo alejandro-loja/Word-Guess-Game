cd66ac9de36a7ebb458cac1d472901b09c1c63bb
Alejandro Loja <alejandro.loja@gmail.com>
2018-09-30 22:46:05 -0400

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,11 @@ var word_to_guess = word_bank[j];
 var good_letter = [];
 var bad_letter = [];
 var dom_word = document.getElementById('word');
+var dom_badletter = document.getElementById('letters-chosen');
 var chances = 6;
 var tries;
 var print_word;
+var lettersChosen;
 
 function print() {
     var word_to_guess = word_bank[j];
@@ -23,7 +25,7 @@ function print() {
             print_word += " _ ";
         }
     }
-    
+
     dom_word.textContent = print_word;
     if (print_word.indexOf('_') === -1) {
         reset('WON');
@@ -31,22 +33,31 @@ function print() {
     }
 }
 
+function lesschance() {
+    chances--;
+    var tries = document.getElementById('tries');
+    tries.textContent = chances;
+    if (chances === 0) {
+        reset('LOST');
+    }
+}
+
+
+
 document.onkeypress = function (event) {
     var word_to_guess = word_bank[j];
     // if key pressed is one (or more) of the letters in the word bank then push that letter to the good letter array.
     if (word_to_guess.indexOf(event.key) > -1) {
         good_letter.push(event.key);
     }
-    else{
+    else if (bad_letter.indexOf(event.key) === -1) {
         bad_letter.push(event.key);
-        chances--;
-        var tries = document.getElementById('tries');
-        tries.textContent = chances;
-        if (chances === 0) {
-            reset('LOST');
-        }
-
+        lesschance();
+        dom_badletter.textContent += " " + event.key + " ";
     }
+    else {
+    }
+
     print();
 }
 //RESET function
@@ -65,6 +76,7 @@ function reset(decision) {
     bad_letter = [];
     good_letter = [];
     chances = 6;
+    dom_badletter.textContent = " ";
 
     print_word = " ";
     alert("You " + decision + "! The Word Was: " + word_to_guess.toUpperCase());
